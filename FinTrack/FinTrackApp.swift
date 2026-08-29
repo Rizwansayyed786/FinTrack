272ca2eb4fdaa5d6317cba38dbf4fc7a565faff3
Rizwan <mh.rizwan582@gmail.com>
2026-08-29 22:48:29 +0530

    Initial Commit

---
+++ FinTrack/FinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinTrackApp.swift
+//  FinTrack
+//
+//  Created by Rizwan N Sayyednavar on 29/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
